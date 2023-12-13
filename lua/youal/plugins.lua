3d5c768e759371e7be5a540db195e8eaf2cf0610
youal <y.oualmakran@gmail.com>
2023-12-14 06:19:47 +0900

    Add back telescope

--- lua/youal/plugins.lua
+++ lua/youal/plugins.lua
@@ -146,26 +146,26 @@ require("lazy").setup({
 	-- 		}
 	-- 	end
 	-- },
-	-- {
-	-- 	'nvim-telescope/telescope.nvim',
-	-- 	tag = '0.1.4',
-	-- 	dependencies = {
-	-- 		'nvim-lua/plenary.nvim',
-	-- 		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	-- 	},
-	-- 	config = function()
-	-- 		require('telescope').setup {}
-	-- 		require('telescope').load_extension('fzf')
-	-- 		-- require("telescope").load_extension "file_browser"
-	-- 		-- require('telescope').load_extension('neoclip')
-	--
-	-- 		local builtin = require('telescope.builtin')
-	-- 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-	-- 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	-- 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-	-- 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-	-- 	end
-	-- },
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		},
+		config = function()
+			require('telescope').setup {}
+			require('telescope').load_extension('fzf')
+			-- require("telescope").load_extension "file_browser"
+			-- require('telescope').load_extension('neoclip')
+
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+		end
+	},
 	-- {
 	-- 	-- "EdenEast/nightfox.nvim",
 	-- 	-- "olimorris/onedarkpro.nvim",
@@ -478,7 +478,7 @@ require("lazy").setup({
 --		'andymass/vim-matchup',
 --		'preservim/tagbar',
 --		'ludovicchabant/vim-gutentags',
-		'mhinz/vim-grepper',
+		-- 'mhinz/vim-grepper',
 		'tpope/vim-fugitive',
 		'tpope/vim-eunuch',
 		'bronson/vim-trailing-whitespace',
